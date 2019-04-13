122303f01b8cd9a048a3ae505a5ae648aff67c96
dswdejonge <dsw.dejonge@gmail.com>
2019-04-13 17:49:39 +0200

    added recursive function, and test

--- coin_change.R
+++ coin_change.R
@@ -1,14 +1,25 @@
 # How many different ways of giving chance given a vector with coins?
-coin_change <- function(coins, change) {
+
+# Solution 1, recursive
+coin_change <- function(coins, change, m){
+  if(change < 0 || m <= 0){
+    return(0)
+  }
+  if(change == 0){
+    return(1)
+  }
+  return(coin_change(coins, change, m - 1) + coin_change(coins, change - coins[m], m))
+}
+
+# Solution 2, brute force
+coin_change2 <- function(coins, change) {
   max_count <- floor(change / coins)
   valid <- which(max_count != 0)
   max_count <- max_count[valid]; coins <- coins[valid]
-  
   coefficients <- list()
   for(i in 1:length(coins)){
     coefficients[[i]] <- 0:max_count[i]
   }
-  
   combis <- expand.grid(coefficients)
   sum <- apply(combis, MARGIN = 1, FUN = function(X){sum(X*coins)})
   return(length(which(sum == change)))
--- coin_change_TEST.R
+++ coin_change_TEST.R
@@ -1,9 +1,19 @@
 source("coin_change.R")
 # Expected 3
-coin_change(
+coin_change2(
   coins = c(1,2),
   change = 4)
 # Expected 1022
 coin_change(
   coins = c(5,10,20,50,100,200,500),
-  change = 300)
+  change = 300,
+  m = 7)
+# Expect 6
+coin_change2(
+  coins = c(1,2,5),
+  change = 7)
+# Expect 31
+coin_change(
+  coins = c(5,10),
+  change = 300,
+  m = 2)
