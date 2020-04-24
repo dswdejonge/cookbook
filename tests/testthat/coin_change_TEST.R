9bd3ba5e2a9bcb04d60b3c29f9c2825d681d0aaf
dswdejonge <dsw.dejonge@gmail.com>
2020-04-24 14:55:33 +0200

    Apply package structure. Test in testthat.

---
+++ tests/testthat/coin_change_TEST.R
@@ -0,0 +1,10 @@
+context("Coin change")
+
+test_that("Coordinates are transformed correctly", {
+  expect_equal(coin_change2(coins = c(1,2),change = 4),3)
+  expect_equal(coin_change(coins = c(5,10,20,50,100,200,500),change = 300,m = 7),1022)
+  expect_equal(coin_change2(coins = c(1,2,5),change = 7),6)
+  expect_equal(coin_change(coins = c(5,10),change = 300,m = 2),31)
+})
+
+
