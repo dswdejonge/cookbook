11889cc0198176c43b96c584ff97028d050cef9b
dswdejonge <dsw.dejonge@gmail.com>
2020-06-21 12:42:47 +0200

    Test for letter values functions.

---
+++ tests/testthat/test_letter_values.R
@@ -0,0 +1,19 @@
+context("Letter values")
+library(cookbook)
+
+test_that("The sum of words are correct", {
+  expect_equal(words_to_marks("abc"),6)
+  expect_equal(words_to_marks("ABC"),6)
+  expect_equal(words_to_marks("AbC"),6)
+})
+
+test_that('add_letters works', {
+  expect_equal(add_letters(c('a', 'b', 'c')), 'f')
+  expect_equal(add_letters(c('z')), 'z')
+  expect_equal(add_letters(c('a', 'b')), 'c')
+  expect_equal(add_letters(c('c')), 'c')
+  expect_equal(add_letters(c('z', 'a')), 'a')
+  expect_equal(add_letters(c('y', 'c', 'b')), 'd')
+  expect_equal(add_letters(c()), 'z')
+})
+
