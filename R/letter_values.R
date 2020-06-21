3f426aa6164576fb17d8cc6f3ddb06f9459fb796
dswdejonge <dsw.dejonge@gmail.com>
2020-06-21 12:42:18 +0200

    Add function to add letters to new letter.

--- R/letter_values.R
+++ R/letter_values.R
@@ -1,3 +1,23 @@
+# Refactored
+letter_value <- function(l){
+  which(tolower(l) == letters)
+}
+
+letter_vector <- function(s){
+  strsplit(s,"")[[1]]
+}
+
+letter_vector_sum <- function(v){
+  sum(sapply(tolower(v), FUN = letter_value))
+}
+
+# Combinations
 words_to_marks <- function(s){
-  sum(sapply(strsplit(s,"")[[1]], FUN = function(x){which(x == letters)}))
+  letter_vector_sum(letter_vector(s))
+}
+
+add_letters <- function(v) {
+  if(length(v) == 0){return("z")}
+  i <- letter_vector_sum(v) %% 26
+  return(letters[ifelse(i == 0, 26, i)])
 }
