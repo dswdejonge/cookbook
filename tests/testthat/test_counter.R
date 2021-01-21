0736dfbdbfb6d1e2901bd9042e374630a98e55df
dswdejonge <dsw.dejonge@gmail.com>
2021-01-21 10:33:47 +0000

    Test counter works with single values.

--- tests/testthat/test_counter.R
+++ tests/testthat/test_counter.R
@@ -7,4 +7,6 @@ testthat::test_that("the counter works", {
   testthat::expect_equal(counter(c(1,3,3), 3), c(2,1,1))
   testthat::expect_equal(counter(c(5,8,8,8), 8), c(6,1,1,1))
   testthat::expect_equal(counter(c(1,5,5), 5), c(2,1,1))
+  testthat::expect_equal(counter(c(1), 5), 2)
+  testthat::expect_equal(counter(c(3), 3), 1)
 })
